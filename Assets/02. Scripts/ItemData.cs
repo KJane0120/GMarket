e7996c43f3e46e3c42ed0e3747e4a9f97980ef4f
lolaop5047 <kthh9828@naver.com>
2025-03-27 17:32:08 +0900

    ✨ Feat: “ItemManager Scripts 추가”

--- Assets/02. Scripts/ItemData.cs
+++ Assets/02. Scripts/ItemData.cs
@@ -2,17 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "New ItemData", menuName = "Items/Stat Item")] //ItemData를 여러개 추가 가능
+[CreateAssetMenu(fileName = "New ItemData", menuName = "Items/StatItem")] //ItemData를 여러개 추가 가능
 public class ItemData : ScriptableObject
 {
-    public string ItemName; //아이템 이름
-    public Sprite ItemIcon; //무기 아이콘UI
-    public WeaponUpgrade[] upgrades; // 무기 강화 단계별 데이터 (배열)
+    public string itemName; //아이템 이름
+    public Sprite itemIcon; //무기 아이콘UI
+    public ItemUpgrade[] upgrades; // 무기 강화 단계별 데이터 (배열)
 
 }
 
 [System.Serializable]  // 개별 강화 데이터
-public class WeaponUpgrade
+public class ItemUpgrade
 {
     public int level;         // 강화 레벨
     public int cost;          // 강화 비용 (필요한 골드)
