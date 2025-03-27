b9d5fbc402d298c08682cc0f3c8aa1337f196854
KJane0120 <rlawoals5755@gmail.com>
2025-03-27 17:02:08 +0900

    ✨ Feat: CurrencyManager.cs 싱글톤 처리
    전반적인 재화 관리를 위해 CurrencyManager.cs를 싱글톤화 했습니다.

---
+++ Assets/02. Scripts/CurrencyManager.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class CurrencyManager : MonoBehaviour
+{
+    private static CurrencyManager instance;
+    public static CurrencyManager Instance { get { return instance; } }
+
+    public CurrencyController controller;
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            if (instance != this)
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+}
