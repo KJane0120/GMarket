8e9c1f54e085c5c81b3a53c07e0b288a09b7c90f
KJane0120 <rlawoals5755@gmail.com>
2025-03-27 22:34:22 +0900

    ✅ Test:: 테스트 코드

--- Assets/02. Scripts/Manager/GameManager.cs
+++ Assets/02. Scripts/Manager/GameManager.cs
@@ -4,10 +4,8 @@ public class GameManager : MonoBehaviour
 {
     private static GameManager instance;
     public static GameManager Instance { get { return instance; } }
-
     public PlayerData PlayerData { get; private set; }
 
-    public CurrencyManager CurrencyManager { get; private set; }
     private void Awake()
     {
         if (instance == null)
@@ -34,16 +32,15 @@ public class GameManager : MonoBehaviour
 
     private void Start()
     {
-        CurrencyManager = new CurrencyManager();
         PlayerData = new PlayerData();
         SetData();
     }
 
     private void SetData() // 데이터 초기화 
     {
-        //Enemy.SetEnemyData; 
         PlayerData.StatGold = CurrencyManager.Instance.controller.statGold;
         PlayerData.WeaponGold = CurrencyManager.Instance.controller.weaponGold;
+        //PlayerData.CurrentWeapon = //기본 무기
     }
 
 
