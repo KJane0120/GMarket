7d600a89974dd62ef36b68db6ea43f1a09c25ae9
lolaop5047 <kthh9828@naver.com>
2025-03-28 10:23:31 +0900

    🙈 WIP: “Item Data Script 수정”

--- Assets/02. Scripts/ItemData.cs
+++ Assets/02. Scripts/ItemData.cs
@@ -7,7 +7,7 @@ public class ItemData : ScriptableObject
 {
     public string itemName; //아이템 이름
     public Sprite itemIcon; //무기 아이콘UI
-    public ItemUpgrade[] upgrades; // 무기 강화 단계별 데이터 (배열)
+    //public ItemUpgrade[] upgrades; // 무기 강화 단계별 데이터 (배열)
 
 }
 
@@ -16,6 +16,6 @@ public class ItemUpgrade
 {
     public int level;         // 강화 레벨
     public int cost;          // 강화 비용 (필요한 골드)
-    public int damage;        // 공격력 증가
+    public int damageUp;        // 공격력 증가
     public int criticalRate;  // 크리티컬 확률 증가
 }
