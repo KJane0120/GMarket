7b3ee20c2183560a0872d08d5bdda57ace5f44bd
KJane0120 <rlawoals5755@gmail.com>
2025-03-28 10:30:02 +0900

    ✨ Feat: AudioManager 싱글톤 처리
    전반적인 오디오 관리를 위한 AudioManager.cs를 싱글톤화했습니다.

---
+++ Assets/02. Scripts/Manager/AudioManager.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    private static AudioManager instance;
+    public static AudioManager Instance { get { return instance; } }
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            if (instance != this)
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+}
