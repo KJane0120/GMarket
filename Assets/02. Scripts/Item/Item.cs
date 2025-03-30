7955d619984e509aa7c4a0e3bbffcc26379e6336
lolaop5047 <kthh9828@naver.com>
2025-03-30 14:19:37 +0900

    ⚡️ Add: “Item Script 추가”

--- Assets/02. Scripts/Item/Item.cs
+++ Assets/02. Scripts/Item/Item.cs
@@ -1,9 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI; //이미지 활성화
+
 
 public class Item : MonoBehaviour
 {
     public ItemData data;
-    public int level
+    public int level;
+    //public Weapon weapon;
+
+    Image icon;
+    TextMeshProUGUI textLevel;
+
+    void Awake()
+    {
+        icon = GetComponentsInChildren<Image>()[1];
+        icon.sprite = data.itemIcon;
+
+        TextMeshProUGUI[] texts = GetComponentsInChildren<TextMeshProUGUI>();
+        textLevel = texts[0];
+    }
+
+    void LateUpdate()
+    {
+        textLevel.text = "Lv." + (level + 1);
+    }
+    
+
 }
