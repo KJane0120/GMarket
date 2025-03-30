c27ba41390ebce0ca502b3a465741c6fe1f45c81
lolaop5047 <kthh9828@naver.com>
2025-03-30 17:17:34 +0900

    ⚡️ Add: “Item LevelUP 기능 추가”
    클릭으로 levelUP 가능
    
    최대 Level 제한 설정

--- Assets/02. Scripts/Item/Item.cs
+++ Assets/02. Scripts/Item/Item.cs
@@ -16,17 +16,45 @@ public class Item : MonoBehaviour
 
     void Awake()
     {
-        icon = GetComponentsInChildren<Image>()[1];
+        icon = GetComponentsInChildren<Image>()[1]; //data Image 불러오기
         icon.sprite = data.itemIcon;
 
-        TextMeshProUGUI[] texts = GetComponentsInChildren<TextMeshProUGUI>();
+        TextMeshProUGUI[] texts = GetComponentsInChildren<TextMeshProUGUI>(); //
         textLevel = texts[0];
     }
 
     void LateUpdate()
     {
-        textLevel.text = "Lv." + (level + 1);
+        textLevel.text = "Lv." + (level + 1); //초기 level 설정
     }
     
+    public void OnClick() //Lv.UP 클릭조건
+    {
+        switch (data.itemType)
+        {
+            case ItemData.ItemType.Melee:
+                break;
+            case ItemData.ItemType.Tooth:
+                break;
+            case ItemData.ItemType.finger:
+                break;
+            case ItemData.ItemType.helmet:
+                break;
+            case ItemData.ItemType.shoes:
+                break;
+            case ItemData.ItemType.Necklace:
+                break;
+        }
+
+        level++; //클릭 할때마다 1UP
+
+        if (level == data.baseDamageUp.Length) //최대 Lv. 초과 방지 코드
+        {
+            GetComponent<Button>().interactable = false;   //Inspector -> Boutton Off
+
+        } 
+
+    }
+
 
 }
