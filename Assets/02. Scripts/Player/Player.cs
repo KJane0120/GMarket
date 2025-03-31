2d9dfea7149e2bada74ab62fa3fe7bc5ab907f62
EunJi <euuu0eeu@gmail.com>
2025-03-31 20:16:16 +0900

    🙈 WIP: “플레이어 일부 수정”
    주석 추가 및 일부 수정

--- Assets/02. Scripts/Player/Player.cs
+++ Assets/02. Scripts/Player/Player.cs
@@ -12,46 +12,54 @@ public class Player : MonoBehaviour
 
     public int levelCritical;           // 치명타 레벨
     public int levelGoldGain;           // 골드획득 레벨
-    public int levelAutoAttacl;         // 자동클릭 레벨
+    public int levelAutoAttack;         // 자동클릭 레벨
 
-    public int totalCritical;           // 치명타 데미지
-    public int totalGoldGain;           // 골드획득량
-    // 자동클릭 부분은 토탈스탯으로 넣을지 말지 고민해보기
-    public int totalAutoAttack;         // 자동클릭 > 클릭이벤트 기능 가져오기, 초당 a회 진행하는식으로 코드 작성
+    public float totalCritical;         // 치명타 데미지
+    public float totalGoldGain;         // 골드획득량
+    public float totalAutoAttack;       // 자동클릭
 
     public int gold;                    // 골드, PlayerData
 
     public void Start()
     {
         gold = GameManager.Instance.PlayerData.StatGold;
-        Init();
+        Init(); // 추후 필요없을경우 삭제
         UpdateTotal();
     }
 
+    /// <summary>
+    /// 플레이어 스탯 초기화 및 PlayerData와 연결
+    /// </summary>
     private void Init()
     {
-        levelCritical = GameManager.Instance.PlayerData.TotalCritDamage;       // 치명타 레벨
-        levelGoldGain = GameManager.Instance.PlayerData.TotalCritDamage;       // 골드획득 레벨
-        levelAutoAttacl = GameManager.Instance.PlayerData.TotalCritDamage;
+        levelCritical = GameManager.Instance.PlayerData.CriticalDamageLevel;        // 치명타 레벨
+        levelGoldGain = GameManager.Instance.PlayerData.GoldGainLevel;              // 골드획득 레벨
+        levelAutoAttack = GameManager.Instance.PlayerData.AutoAttackLevel;          // 자동공격 레벨
 
-        totalCritical = GameManager.Instance.PlayerData.TotalCritDamage;
-        totalGoldGain = GameManager.Instance.PlayerData.TotalGoldGain;
+        totalCritical = GameManager.Instance.PlayerData.TotalCritDamage;            // 최종 치명타 데미지
+        totalGoldGain = GameManager.Instance.PlayerData.TotalGoldGain;              // 최종 골드획득량
+        totalAutoAttack = GameManager.Instance.PlayerData.totalAutoAttack;          // 최종 자동클릭횟수
     }
 
     /// <summary>
-    /// 스탯 업데이트
+    /// 스탯 업데이트 및 타입 분류
     /// </summary>
     public void UpdateTotal()
     {
-        levelCritical = LevelStat(StatType.critical);       // 치명타 레벨
-        levelGoldGain = LevelStat(StatType.goldGain);        // 골드획득 레벨
-        levelAutoAttacl = LevelStat(StatType.autoAttack);
+        levelCritical = LevelStat(StatType.critical);
+        levelGoldGain = LevelStat(StatType.goldGain);
+        levelAutoAttack = LevelStat(StatType.autoAttack);
 
         totalCritical = BonusStat(BonusStatType.criticalBonus);
         totalGoldGain = BonusStat(BonusStatType.goldGainBonus);
-        //totalAutoAttack = BonusStat(BonusStatType.autoAttackBonus); 
+        totalAutoAttack = BonusStat(BonusStatType.autoAttackBonus); 
     }
 
+    /// <summary>
+    /// 스탯 레벨
+    /// </summary>
+    /// <param name="statType"></param>
+    /// <returns></returns>
     public int LevelStat (StatType statType)
     {
         float levelValue = 0f;
@@ -74,16 +82,14 @@ public class Player : MonoBehaviour
 
 
     /// <summary>
-    /// 토탈 스탯, 최종은 PlayerData로
+    /// 보너스 스탯
     /// </summary>
     /// <param name="statType"></param>
     /// <returns></returns>
-    // 추후 int > float로 바꿔주기
-    public int BonusStat(BonusStatType bonusType)
+    public float BonusStat(BonusStatType bonusType)
     {
         float bonusValue = 0f;
 
-        // 보너스스탯
         switch (bonusType)
         {
             case BonusStatType.criticalBonus:
@@ -92,9 +98,9 @@ public class Player : MonoBehaviour
             case BonusStatType.goldGainBonus:
                 bonusValue = playerStat.addStat.bonusValue;
                 return GameManager.Instance.PlayerData.TotalGoldGain;
-            //case BonusStatType.autoAttackBonus:
-            //    bonusValue = playerStat.addStat.bonusValue;
-            //    return GameManager.Instance.PlayerData.TotalCritChance;
+            case BonusStatType.autoAttackBonus:
+                bonusValue = playerStat.addStat.bonusValue;
+                return GameManager.Instance.PlayerData.TotalCritChance;
             default:
                 return 0;
         }
--- Assets/02. Scripts/Player/PlayerUpgrade.cs
+++ Assets/02. Scripts/Player/PlayerUpgrade.cs
@@ -8,8 +8,8 @@ public class PlayerUpgrade : MonoBehaviour
 {
     public Player player;
     public PlayerStat playerStat;
+    public ClickManager clickManager;
 
-    //public TMP_Text goldText;
     public TMP_Text curStatText;
     public TMP_Text upStatText;
     public TMP_Text upGoldText;
@@ -23,7 +23,6 @@ public class PlayerUpgrade : MonoBehaviour
 
     void Start()
     {
-        //player.UpdateTotal();
         Gold = GameManager.Instance.PlayerData.StatGold;
         UpdateUI();
     }
@@ -35,18 +34,15 @@ public class PlayerUpgrade : MonoBehaviour
     {
         int gold = playerStat.upgradeGold; // 강화 비용 가져오기
 
-        // player.gold 이부분 나중에 playerData에 있는걸로 가져오기
         if (Gold >= gold)
         {
             CurrencyManager.Instance.controller.StatGoldUse(gold);
-            playerStat.UpgradeBonus();      // 보너스 스탯증가
-            //player.UpdateTotal();           // 토탈 스탯 - 추후 내용 변경되면 수정
+            playerStat.UpgradeBonus();
             UpdateUI();
         }
         else
         {
             UIManager.Instance.StatsErrorMsg();
-            // 추후 ui부분 넣기
             Debug.Log("골드오링");
         }
     }
@@ -56,11 +52,10 @@ public class PlayerUpgrade : MonoBehaviour
     /// </summary>
     void UpdateUI()
     {
-        //goldText.text = $"{player.gold}";                       // 보유골드- 나중에 다른곳으로 빼기
         curStatText.text = $"{playerStat.stat.statValue}";      // 현 스탯
         upStatText.text = $"{playerStat.addStat.bonusValue}";   // 보너스 스탯
         upGoldText.text = $"{playerStat.upgradeGold}";          // 업그레이드 골드
-        //totalText.text = $"+{player.totalCritical}%";          // 합산 치명타 스탯, 다른 스탯도 동일하게 - 추후 토탈스탯 관리하는쪽으로 이동예정
+        //totalText.text = $"+{player.totalCritical}%";         // 토탈스탯 - 추후 이동예정
     }
 
     // 버튼 이벤트트리거 - 누르는 순간 실행
@@ -76,18 +71,19 @@ public class PlayerUpgrade : MonoBehaviour
     // 버튼 이벤트트리거 - 떼는 순간 실행
     public void OnButtonUp()
     {
-        isHolding = false; // false로 변경
-        if (upgradeCoroutine != null) // 실행중인 코루틴이 있으면
+        isHolding = false;                      // false로 변경
+        if (upgradeCoroutine != null)           // 실행중인 코루틴이 있으면
         {
-            StopCoroutine(upgradeCoroutine); // 코루틴 중지
-            upgradeCoroutine = null; // 변수 초기화
+            StopCoroutine(upgradeCoroutine);    // 코루틴 중지
+            upgradeCoroutine = null;            // 변수 초기화
         }
     }
 
-    // 반복실행
+    // 이벤트트리거 - 반복실행
     private IEnumerator UpgradeLoop()
     {
-        //yield return new WaitForSeconds(delayTime);
+        // 일반 클릭과 동시에 진행되지 않게 기다리게 함
+        yield return new WaitForSeconds(delayTime);
 
         while (isHolding) // 버튼을 누르고있는동안 실행
         {
