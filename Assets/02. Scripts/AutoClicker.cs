c47948c994c7eddd44df794a29c5a5b4e841119e
AAAAAA123222 <kdo03122@gmail.com>
2025-04-01 21:56:47 +0900

    Feat: ClickManager의 UI활성화 여부 확인 및 자동공격 변수 연결

--- Assets/02. Scripts/AutoClicker.cs
+++ Assets/02. Scripts/AutoClicker.cs
@@ -5,21 +5,9 @@ public class AutoClicker : MonoBehaviour
 {
     private ClickManager clickManager;
     private Coroutine autoClickCoroutine;
-    public float AALevel;
-
     private void Start()
     {
         clickManager = FindObjectOfType<ClickManager>();
-
-        UpAutoAttack();
-    }
-
-    private void Update()
-    {
-        if (AALevel > 0)
-        {
-            StartAutoClick();
-        }
     }
 
     public void StartAutoClick()
@@ -34,13 +22,16 @@ public class AutoClicker : MonoBehaviour
     {
         while (true)
         {
-            float attackRate = AALevel * 0.3f; // 1레벨당 0.3번 공격
+            float attackRate = GameManager.Instance.PlayerData.AutoAttackLevel * 0.3f; // 1레벨당 0.3번 공격
             if (attackRate > 0)
             {
                 float interval = 1f / attackRate; // 공격 간격 설정
+                if (!(clickManager.isGamePaused))
+                {
 
-                Debug.Log("자동 공격");
-                clickManager.onClick.Invoke();
+                    Debug.Log("자동 공격");
+                    clickManager.onClick.Invoke();
+                }
 
                 yield return new WaitForSeconds(interval);
             }
@@ -53,9 +44,8 @@ public class AutoClicker : MonoBehaviour
 
     public void UpAutoAttack()
     {
-        //AALevel++;
         GameManager.Instance.PlayerData.AutoAttackLevel++;
-        if (AALevel > 0)
+        if (GameManager.Instance.PlayerData.AutoAttackLevel > 0)
         {
             StartAutoClick();
         }
--- Assets/02. Scripts/Manager/ClickManager.cs
+++ Assets/02. Scripts/Manager/ClickManager.cs
@@ -4,17 +4,29 @@ using UnityEngine.Events;
 public class ClickManager : MonoBehaviour
 {
     public Enemy targetEnemy;  // 타겟 적
-    public bool isGamePaused = false;
+    public bool isGamePaused;
     public UnityAction onClick;
     private AutoClicker autoClicker; // AutoClicker 참조
+
+    public GameObject UIPrefab;
 
     private void Start()
     {
         autoClicker = FindObjectOfType<AutoClicker>(); // AutoClicker 찾기
     }
 
+
     private void Update()
     {
+        if (UIManager.Instance.inventoryPanel.activeInHierarchy || UIManager.Instance.PausePopup.activeInHierarchy)
+        {
+            isGamePaused = true;
+        }
+        else
+        {
+            isGamePaused = false;
+        }
+
         if (isGamePaused) return;
 
         // 마우스 클릭 감지
