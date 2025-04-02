5406ead9ce3b9a102500b034d6f25f58be1bf314
k6931k <k6931k@naver.com>
2025-04-02 15:03:47 +0900

    ✨ Feat: “UI클릭 무시기능 추가”

--- Assets/02. Scripts/Manager/ClickManager.cs
+++ Assets/02. Scripts/Manager/ClickManager.cs
@@ -1,5 +1,7 @@
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.EventSystems;
 
 public class ClickManager : MonoBehaviour
 {
@@ -9,6 +11,8 @@ public class ClickManager : MonoBehaviour
     private AutoClicker autoClicker; // AutoClicker 참조
 
     public GameObject UIPrefab;
+
+    private RectTransform WindowUI;
 
     private void Start()
     {
@@ -32,7 +36,19 @@ public class ClickManager : MonoBehaviour
         // 마우스 클릭 감지
         if (Input.GetMouseButtonDown(0))
         {
+            //IndowUI 렉트트랜스폼을 찾은 뒤, 그 안에 마우스가 있는지 확인
+            WindowUI = UIManager.Instance.transform.Find("WindowUI").GetComponent<RectTransform>();
+            bool isMouseOnUI = RectTransformUtility.RectangleContainsScreenPoint(WindowUI, Input.mousePosition);
+            
+            // 만약 있다면, 실행시키지 않고 돌아가기
+            if (isMouseOnUI)
+            {
+                //onClick 이벤트를 수행하지 않고 그냥 돌아가기
+                return;
+            }
             onClick.Invoke(); // 공격 실행
+            Debug.Log("공격 감지");
         }
     }
+    
 }
